ca08403128e91bd691b42f5ab4fe0a55956e125f
Juho Yrjölä <juho.yrjola@iki.fi>
2021-07-23 22:54:15 +0300

    Continue developing agent messaging

--- src/agents/agents/TransactionReceiverAgent.java
+++ src/agents/agents/TransactionReceiverAgent.java
@@ -7,7 +7,11 @@ import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.DFService;
 import jade.domain.FIPAException;
+import jade.lang.acl.MessageTemplate;
 import jade.util.Logger;
+
+import java.util.UUID;
+import util.LNPaymentProtocol;
 
 public class TransactionReceiverAgent extends Agent{
 
@@ -30,22 +34,136 @@ public class TransactionReceiverAgent extends Agent{
         }
     }
 
-    private class TransactReceiveBehaviour extends CyclicBehaviour {
+    private class TransactReceiveBehaviour extends Behaviour {
+
+        private AID senderAgent; //sender
+
+        private String currency; //base currency
+        private double valCurr; //value in base currency
+        private int valSats; //value in satoshis
+        private String prodID; //product id of the transaction
+
+        //State of the protocol. Internal to this agent, does not correspond to the state of the counterparty.
+        //0 = Not initiated
+        //1 = Proposal rejected
+        //2 = Proposal accepted
+        //3 = Invoice should be paid
+        //4 = Successfully finished
+        //5 = Failure
+        private int state;
+
+        private UUID convId;
+
+        //template for replies, updated on every state
+        private MessageTemplate replyTemplate;
+
+        //template with convId and protocol
+        private MessageTemplate convBaseTemplate;
 
         public TransactReceiveBehaviour(Agent a) {
             super(a);
+
+            //first template: performative and protocol
+            this.replyTemplate = MessageTemplate.and(
+                    MessageTemplate.MatchPerformative(ACLMessage.PROPOSE),
+                    MessageTemplate.MatchProtocol(LNPaymentProtocol.getProtocolName()));
+
+            this.state = 0;
         }
 
         public void action() {
-            ACLMessage  msg = myAgent.receive();
+            ACLMessage msgIn = myAgent.receive(replyTemplate);
 
-            if(msg != null){
-                String content = msg.getContent();
-                myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Received message: "+content);
-            }else{
-                block();
+            switch (state) {
+                case 0:
+                    if (msgIn != null) {
+
+                        //validate the initiation message
+                        if (msgIn.getConversationId().isEmpty()) {
+                            myLogger.log(Logger.WARNING, "Receiver Agent: Initiation message doesn't have a conversation id!");
+                            block();
+                        }
+                        //-currency
+                        //-product
+                        //-sats value
+                        //-currency value
+
+                        convId = UUID.fromString(msgIn.getConversationId());
+
+                        senderAgent = msgIn.getSender();
+
+
+                        String content = msgIn.getContent();
+                        myLogger.log(Logger.INFO, "Agent " + getLocalName() + " - Received message: " + content);
+
+                        //IF ACCEPT
+                        boolean acceptProposal = true;
+                        if (acceptProposal) {
+                            ACLMessage accept = new ACLMessage(ACLMessage.ACCEPT_PROPOSAL);
+                            accept.addReceiver(senderAgent);
+                            accept.setProtocol(LNPaymentProtocol.getProtocolName());
+                            accept.setConversationId(convId.toString());
+                            if (!msgIn.getReplyWith().isEmpty()) {
+                                accept.setInReplyTo(msgIn.getReplyWith());
+                            }
+                            accept.setReplyWith("accept" + System.currentTimeMillis()); // Unique value
+
+                            accept.setContent("TEST ACCEPT");
+
+                            myLogger.log(Logger.INFO, "MESSAGE: "+accept.toString());
+
+                            myAgent.send(accept);
+
+                            convBaseTemplate = MessageTemplate.and(
+                                    MessageTemplate.MatchProtocol(LNPaymentProtocol.getProtocolName()),
+                                    MessageTemplate.MatchConversationId(convId.toString()));
+
+                            state = 1;
+                        } else {
+                            //reject
+
+                            //....
+                            myLogger.log(Logger.INFO, "reject");
+
+                        }
+
+                    } else {
+                        block();
+                    }
+
+                    break;
+                case 1:
+                    //myLogger.log(Logger.INFO, "Receiver state 1");
+                    break;
+                case 2:
+                    //myLogger.log(Logger.INFO, "Receiver state 2");
+                    break;
+                case 3:
+                    //myLogger.log(Logger.INFO, "Receiver state 3");
+                    break;
+                case 4:
+                    //myLogger.log(Logger.INFO, "Receiver state 4");
+                    break;
+
+
+            }
+        }
+
+        public boolean done() {
+
+            //end
+            if (state == 5) {
+                myLogger.log(Logger.WARNING, "Transaction failed.");
+                return true;
+            }
+            if (state == 4) {
+                myLogger.log(Logger.WARNING, "Transaction completed successfully.");
+                return true;
             }
 
+            //continue
+            return false;
         }
+
     }
 }
--- src/agents/agents/TransactionSenderAgent.java
+++ src/agents/agents/TransactionSenderAgent.java
@@ -9,7 +9,11 @@ import jade.domain.FIPAAgentManagement.DFAgentDescription;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 import jade.util.Logger;
+
+import java.util.UUID;
+import util.LNPaymentProtocol;
 
 //The transaction sender agent initiates the conversation
 public class TransactionSenderAgent extends Agent {
@@ -27,51 +31,108 @@ public class TransactionSenderAgent extends Agent {
 
     private class TransactSendBehaviour extends Behaviour {
 
-        Agent a; //this agent
-        AID receiverAgent; //receiver
+        private AID receiverAgent; //receiver
 
-        String currency; //base currency
-        double valCurr; //value in base currency
-        int valSats; //value in satoshis
-        String prodID; //product id for the receiver
+        private String currency; //base currency
+        private double valCurr; //value in base currency
+        private int valSats; //value in satoshis
+        private String prodID; //product id for the receiver
+
+        //State of the protocol. Internal to this agent, does not correspond to the state of the counterparty.
+        //0 = Not initiated
+        //1 = Waiting for response to proposal
+        //2 = Proposal rejected
+        //3 = Proposal accepted
+        //4 = Successfully finished
+        //5 = Failure
+        private int state;
+
+        private UUID convId;
+
+        //template for replies, updated on every state
+        private MessageTemplate replyTemplate;
+
+        private MessageTemplate convBaseTemplate;
+
 
         public TransactSendBehaviour(Agent a, AID receiverAgent, String currency, double valCurr, int valSats, String prodID) {
             super(a);
-            this.a = a;
             this.receiverAgent = receiverAgent;
             this.currency = currency;
             this.valCurr = valCurr;
             this.valSats = valSats;
             this.prodID = prodID;
+
+            this.state = 0;
+            this.convId = UUID.randomUUID(); //set the conversation id
+            this.convBaseTemplate = MessageTemplate.and(
+                    MessageTemplate.MatchProtocol(LNPaymentProtocol.getProtocolName()),
+                    MessageTemplate.MatchConversationId(convId.toString()));
         }
 
         public void action() {
 
-            /*ACLMessage msg = myAgent.receive();
+            ACLMessage msgIn = myAgent.receive(replyTemplate);
 
-            if(msg != null){
-                String content = msg.getContent();
-                myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Received message: "+content);
-            }else{
-                block();
-            }*/
+            switch (state) {
+                case 0:
+                    ACLMessage propose = new ACLMessage(ACLMessage.PROPOSE);
+                    propose.addReceiver(this.receiverAgent);
 
-            //TEST SEND
-            ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-            msg.addReceiver(this.receiverAgent);
-            msg.setContent(this.prodID);
-            //msg.setConversationId("book-trade");
-            //msg.setReplyWith("cfp"+System.currentTimeMillis()); // Unique value
-            myAgent.send(msg);
+                    propose.setContent(this.prodID); //MUUTA OIKEA DATA
+
+                    propose.setConversationId(convId.toString());
+                    propose.setProtocol(LNPaymentProtocol.getProtocolName());
+                    propose.setReplyWith("propose"+System.currentTimeMillis()); // Unique value
+                    myAgent.send(propose);
+
+                    //template: convId and (accept_proposal or reject_proposal) and replywith
+                    replyTemplate =  MessageTemplate.and(convBaseTemplate,
+                            MessageTemplate.and(
+                                MessageTemplate.or(MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL), MessageTemplate.MatchPerformative(ACLMessage.REJECT_PROPOSAL)),
+                                MessageTemplate.MatchInReplyTo(propose.getReplyWith())));
+
+                    state = 1;
+                    break;
+                case 1:
+                    if(msgIn != null){
+                        String content = msgIn.getContent();
+                        myLogger.log(Logger.INFO, "Agent "+getLocalName()+" - Received ACCEPT / REJECT");
+                    }else{
+                        block();
+                    }
+                    break;
+                case 2:
+                    //myLogger.log(Logger.INFO, "Sender state 2");
+                    break;
+                case 3:
+                    //myLogger.log(Logger.INFO, "Sender state 3");
+                    break;
+                case 4:
+                    //myLogger.log(Logger.INFO, "Sender state 4");
+                    break;
+                case 5:
+                    //myLogger.log(Logger.INFO, "Sender state 4");
+                    break;
+
+            }
+
         }
 
         public boolean done() {
 
             //end
-            return true;
+            if (state == 5) {
+                myLogger.log(Logger.WARNING, "Transaction failed.");
+                return true;
+            }
+            if (state == 4) {
+                myLogger.log(Logger.WARNING, "Transaction completed successfully.");
+                return true;
+            }
 
             //continue
-            //return false;
+            return false;
         }
 
     }
@@ -96,7 +157,7 @@ public class TransactionSenderAgent extends Agent {
             //SLEEP TO MAKE SURE THAT THE RECEIVER AGENT IS CREATED
             try
             {
-                Thread.sleep(2000);
+                Thread.sleep(10000);
             }
             catch(InterruptedException ex)
             {
